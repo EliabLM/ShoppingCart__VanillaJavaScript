7fdd784e26e9b8923971db20d1d81ead00f9fc7b
eliablm <eliablopez@hotmail.com>
2021-06-25 14:00:41 -0500

    button and cart added

--- app.js
+++ app.js
@@ -1,32 +1,66 @@
 const items = document.getElementById("items");
-const templateCard = document.getElementById("template-card").content
-const fragment = document.createDocumentFragment()
+const templateCard = document.getElementById("template-card").content;
+const fragment = document.createDocumentFragment();
+let carrito = {};
 
 document.addEventListener("DOMContentLoaded", () => {
-    fetchData()
-})
+	fetchData();
+});
 
-const fetchData =  async () => {
-    try {
-        const res = await fetch("./api.json")
-        const data = await res.json()
-        // console.log(data)
-        pintarCards(data)
-    } catch (error) {
-        console.log(error)
-    }
-}
+items.addEventListener("click", (event) => {
+	addcarrito(event);
+});
 
-const pintarCards = data => {
-    data.forEach(producto => {
-        templateCard.querySelector("h5").textContent = producto.title
-        templateCard.querySelector('p').textContent = `$${producto.precio}`
-        templateCard.querySelector("img").setAttribute("src", producto.thumbnailUrl)
-        templateCard.querySelector(".btn-dark").dataset.id = producto.id
+const fetchData = async () => {
+	try {
+		const res = await fetch("./api.json");
+		const data = await res.json();
+		// console.log(data)
+		pintarCards(data);
+	} catch (error) {
+		console.log(error);
+	}
+};
 
-        // Investigar sobre clone
-        const clone = templateCard.cloneNode(true)
-        fragment.appendChild(clone)
-    });
-    items.appendChild(fragment)
-}
+const pintarCards = (data) => {
+	data.forEach((producto) => {
+		templateCard.querySelector("h5").textContent = producto.title;
+		templateCard.querySelector("p").textContent = `$${producto.precio}`;
+		templateCard
+			.querySelector("img")
+			.setAttribute("src", producto.thumbnailUrl);
+		templateCard.querySelector(".btn-dark").dataset.id = producto.id;
+
+		// Investigar sobre clone
+		const clone = templateCard.cloneNode(true);
+		fragment.appendChild(clone);
+	});
+	items.appendChild(fragment);
+};
+
+const addcarrito = (event) => {
+	// console.log(event.target);
+	// console.log(event.target.classList.contains("btn-dark"));
+	if (event.target.classList.contains("btn-dark")) {
+		setCarrito(event.target.parentElement);
+	}
+	event.stopPropagation();
+};
+
+const setCarrito = (objeto) => {
+	// console.log(objeto);
+	const producto = {
+		id: objeto.querySelector(".btn-dark").dataset.id,
+		title: objeto.querySelector("h5").textContent,
+		title: objeto.querySelector("p").textContent,
+		cantidad: 1,
+	};
+
+	if (carrito.hasOwnProperty(producto.id)) {
+		producto.cantidad = carrito[producto.id].cantidad + 1;
+	}
+
+	carrito[producto.id] = { ...producto };
+
+	console.log(carrito);
+};
